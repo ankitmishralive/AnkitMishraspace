225e13c631d38cdc21680e1cbfe89853b451274b
Ankit <ankitxtowod@gmail.com>
2023-12-06 01:27:36 +0530

    updated

--- routes.py
+++ routes.py
@@ -3,5 +3,8 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('home.html', title='Flask Starter Template')
+    return render_template('home.html')
     # return "heyy wassup"
+@app.route('/showcase')
+def showcase():
+    return render_template('showcase/home.html')
---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+function bottom(e)
+{
+let listthree = document.querySelector("#getintouch");
+ listthree.scrollIntoView({behavior:"smooth"});
+}
+
+
+function redirectToShowcase() {
+    window.location.href = "/showcase";
+}
